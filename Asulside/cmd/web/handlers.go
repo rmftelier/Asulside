817eb6a35d3d4c701acd2bd2ad03469013ac0cf5
Roberta <63109114+rmftelier@users.noreply.github.com>
2022-06-03 03:42:35 +0000

    5. Loggers, flags e DI

--- Asulside/cmd/web/handlers.go
+++ Asulside/cmd/web/handlers.go
@@ -5,10 +5,9 @@ import (
 	"net/http"
 	"strconv"
   "html/template"
-  "log"
 )
 
-func home(rw http.ResponseWriter, r *http.Request){
+func(app *application) home(rw http.ResponseWriter, r *http.Request){
   if r.URL.Path != "/"{
     http.NotFound(rw, r)
     return
@@ -23,20 +22,20 @@ func home(rw http.ResponseWriter, r *http.Request){
 
   ts, err := template.ParseFiles(files...)
   if err != nil{
-      log.Println(err.Error())
+      app.errorLog.Println(err.Error())
       http.Error(rw, "Internal Error", 500)
      return
   }
   err = ts.Execute(rw, nil)
   if err != nil{
-       log.Println(err.Error())
+       app.errorLog.Println(err.Error())
        http.Error(rw, "Internal Error", 500)
        return
   }
 }
 
 //http://localhost:4000/snippet?id=123
-func showBlog(rw http.ResponseWriter, r *http.Request){
+func(app *application) showBlog(rw http.ResponseWriter, r *http.Request){
   id,err := strconv.Atoi(r.URL.Query().Get("id"))
   if err != nil || id < 1 {
     http.NotFound(rw, r)
@@ -45,7 +44,7 @@ func showBlog(rw http.ResponseWriter, r *http.Request){
   fmt.Fprintf(rw, "Exibir o Blog de ID: %d", id)
 }
 
-func createBlog(rw http.ResponseWriter, r *http.Request){
+func(app *application) createBlog(rw http.ResponseWriter, r *http.Request){
   if r.Method != "POST"{
     rw.Header().Set("Allow","POST")
     http.Error(rw, "Metodo não permitido", http.StatusMethodNotAllowed)
--- Asulside/cmd/web/main.go
+++ Asulside/cmd/web/main.go
@@ -1,23 +1,49 @@
 package main
 
 import (
+  "flag"
 	"log"
 	"net/http"
+  "os"
 )
+
+type application struct{
+     errorLog *log.Logger
+     infoLog *log.Logger
+}
+
 
 //Conexão ao servidor
 func main() {
+  
+  //nome da flag, valor padrão e descrição
+   addr := flag.String("addr", ":4000", "Porta da Rede")
+   flag.Parse()
+
+   infoLog := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
+  errorLog := log.New(os.Stderr, "ERRO:\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+
+//application
+app := &application{
+    errorLog: errorLog,
+    infoLog: infoLog,
+}
+
+  //conexão ao servidor
 	mux := http.NewServeMux()
 
-  mux.HandleFunc("/", home)
-  mux.HandleFunc("/blog", showBlog)
-  mux.HandleFunc("/editor", createBlog)
+  mux.HandleFunc("/", app.home)
+  mux.HandleFunc("/blog", app.showBlog)
+  mux.HandleFunc("/editor", app.createBlog)
 
   //
   fileServer := http.FileServer(http.Dir("./ui/static/"))
   mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+
+  infoLog.Printf("Inicializando o servidor na porta %s\n", *addr)
   
-  log.Println("Inicializando o servidor na porta: 4000")
-  err := http.ListenAndServe(":4000", mux)
-  log.Fatal(err)
+  err := http.ListenAndServe(*addr, mux) 
+  errorLog.Fatal(err)
 }
