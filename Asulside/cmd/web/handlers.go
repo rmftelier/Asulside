11015a16561b8766861f62578561e7e0e6cd2864
Roberta <63109114+rmftelier@users.noreply.github.com>
2022-06-05 22:12:58 +0000

    7.3. Está funcionando

--- Asulside/cmd/web/handlers.go
+++ Asulside/cmd/web/handlers.go
@@ -4,16 +4,9 @@ import (
 	"net/http"
 	"strconv"
   "html/template"
- // "fmt"
- // "os"
-  //"time"
-  //"path/filepath"
-  //"io"
 
   "github.com/rmftelier/Asulside/pkg/models"
 )
-
-const MAX_UPLOAD_SIZE = 1024 * 1024 //mudei aqui
 
 func(app *application) home(rw http.ResponseWriter, r *http.Request){
   if r.URL.Path != "/"{
@@ -45,7 +38,6 @@ func(app *application) home(rw http.ResponseWriter, r *http.Request){
   }
 }
 
-//http://localhost:4000/snippet?id=1
 func(app *application) showBlog(rw http.ResponseWriter, r *http.Request){
   id,err := strconv.Atoi(r.URL.Query().Get("id"))
   if err != nil || id < 1 {
@@ -82,9 +74,8 @@ func(app *application) showBlog(rw http.ResponseWriter, r *http.Request){
 
 func(app *application) createBlog(rw http.ResponseWriter, r *http.Request){
 
-  
-  //Testando a partir daqui -> 04/06 kkakakak
-  	if r.Method == "GET" {
+     //Dúvida aqui 
+    	if r.Method == "GET" {
 		tmpl, _ := template.ParseFiles("./ui/html/editor.html")
 
 		tmpl.Execute(rw, "")
@@ -96,70 +87,12 @@ func(app *application) createBlog(rw http.ResponseWriter, r *http.Request){
 		http.Error(rw, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
+
+*/
   
 
   
-	// 32 MB is the default used by FormFile
-	if err := r.ParseMultipartForm(32 << 20); err != nil {
-		http.Error(rw, err.Error(), http.StatusBadRequest)
-		return
-	}
 
-	// get a reference to the fileHeaders
-	files := r.MultipartForm.File["file"]
-
-	for _, fileHeader := range files {
-		if fileHeader.Size > MAX_UPLOAD_SIZE {
-			http.Error(rw, fmt.Sprintf("The uploaded image is too big: %s. Please use an image less than 1MB in size", fileHeader.Filename), http.StatusBadRequest)
-			return
-		}
-
-		file, err := fileHeader.Open()
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		defer file.Close()
-
-		buff := make([]byte, 512)
-		_, err = file.Read(buff)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		filetype := http.DetectContentType(buff)
-		if filetype != "image/jpeg" && filetype != "image/png" {
-			http.Error(rw, "The provided file format is not allowed. Please upload a JPEG or PNG image", http.StatusBadRequest)
-			return
-		}
-
-		_, err = file.Seek(0, io.SeekStart)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-    
-		err = os.MkdirAll("./ui/uploads", os.ModePerm)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		f, err := os.Create(fmt.Sprintf("./ui/uploads/%d%s", time.Now().UnixNano(), filepath.Ext(fileHeader.Filename)))
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		defer f.Close()
-	}
-
-	fmt.Fprintf(rw, "Upload successful")
-
- */
 } 
 
 
--- Asulside/pkg/models/mysql/blogs.go
+++ Asulside/pkg/models/mysql/blogs.go
@@ -7,7 +7,6 @@ import (
 
 )
 
-
 type BlogModel struct{
      DB *sql.DB
 }
